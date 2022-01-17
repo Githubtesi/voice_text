230d8880de9eb89ebd51023f8b55a21d8e84f039
Githubtesi <tesiyosi7211@gmail.com>
2022-01-18 02:43:28 +0900

    キーボードでディスプレイ起動

--- controllers/key_listen.py
+++ controllers/key_listen.py
@@ -2,20 +2,17 @@ from pynput.keyboard import Key, Listener
 
 import views.display
 
-is_show = True
-is_not_show = not is_show
 
 
 def on_press(key):
     print('{0} pressed'.format(key))
-    if key == Key.ctrl_r:
-        views.display.init_display()
 
 
 def on_release(key):
     print('{0} release'.format(key))
     if key == Key.ctrl_r:
-        views.display.close_window()
+        # 画面表示
+        views.display.init_display()
 
     if key == Key.esc:
         # Stop listener
